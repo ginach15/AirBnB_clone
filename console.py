5ef85eaae0e3198e6291ec194c19425c185c812f
ginach15 <ginach1215@gmail.com>
2023-02-12 01:58:42 -0800

    console

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/user/bin/python3
+
 """Module for the entry point of the command interpreter."""
 
 import cmd
@@ -212,4 +213,5 @@ class HBNBCommand(cmd.Cmd):
                 storage.all()[key].save()
 
 if __name__ == '__main__':
+
     HBNBCommand().cmdloop()
